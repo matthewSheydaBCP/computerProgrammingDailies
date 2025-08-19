664815260a6cdad90eb91c9498ab42c5bac1f7f4
matthewSheydaBCP <matthew.sheyda28@bcp.org>
2025-08-19 14:30:54 -0700

    Update day1.py
    Its a const now :D

--- day1.py
+++ day1.py
@@ -1,9 +1,9 @@
 import random
 
-bestTeamEver = ["Tom Brady", "Steph Curry", "Victor Tsoi"]
+BEST_TEAM_EVA = ["Tom Brady", "Steph Curry", "Victor Tsoi"]
 
 def getRandomPlayer(playerList):
     return playerList[random.randint(0,len(playerList)-1)] 
     #Returns element of playerList array at a random index between 0 and len(playerList).
 
-print(getRandomPlayer(bestTeamEver))
+print(getRandomPlayer(BEST_TEAM_EVA))
